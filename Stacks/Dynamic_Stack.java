6b672103ec907893dd31d15f5a97cdd273055ba4
Thenx0009 <ayushverm0009@gmail.com>
2023-08-01 23:57:09 +0530

    initial commit

--- Stacks/Dynamic_Stack.java
+++ Stacks/Dynamic_Stack.java
@@ -1,8 +1,17 @@
 package Stacks;
 
+import Stacks.Stack;
+
 public class Dynamic_Stack extends Stack{
 	
-	public void push(int item) {
-		if(isfull())
+	public void push(int item)throws Exception {
+		if (super.isfull()) {
+			int[] newarr = new int[2 * arr.length];
+			for (int i = 0; i < arr.length; i++) {
+				newarr[i] = arr[i];
+			}
+			arr = newarr;
+		}
+		super.push(item);
 	}
 }
