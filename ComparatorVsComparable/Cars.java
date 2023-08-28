9703aeb70e12d63628e78fa1ddbc56e2dd89b7dc
Thenx0009 <138437609+Thenx0009@users.noreply.github.com>
2023-08-28 22:57:44 +0530

    Update Cars.java

--- ComparatorVsComparable/Cars.java
+++ ComparatorVsComparable/Cars.java
@@ -23,9 +23,9 @@ public class Cars implements Comparable<Cars> {
 	@Override
 	public int compareTo(Cars o) {
 		// TODO Auto-generated method stub
-		//return this.speed - o.speed;//sorting on the basis of speed
-		//return o.price-this.price;//sorting on the basis of price
-		return this.color.compareTo(o.color);
+		//return this.speed - o.speed;  //sorting on the basis of speed
+		//return o.price-this.price;    //sorting on the basis of price
+		return this.color.compareTo(o.color);   //sorting on the basis of color
 	}
 	
 
