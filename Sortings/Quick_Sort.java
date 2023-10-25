68cd43a98625fb0b8cfc5c5c44c6cf0f1c0f83d2
Thenx0009 <ayushverm0009@gmail.com>
2023-10-25 12:25:01 +0530

    Initial Commit

--- Sortings/Quick_Sort.java
+++ Sortings/Quick_Sort.java
@@ -4,7 +4,39 @@ public class Quick_Sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int arr[] = { 7, 5, 3, 1, 9, 4 };
+		Quick_sort(arr, 0, arr.length - 1);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 
+	public static void Quick_sort(int arr[], int low, int high) {
+		if (low >= high) {
+			return;
+		}
+		int idx = partition(arr, low, high);
+		Quick_sort(arr, low, idx - 1);
+		Quick_sort(arr, idx + 1, high);
+	}
+
+	public static int partition(int arr[], int low, int high) {
+		int lastelement = arr[high];
+		int idx = 0;
+		for (int i = 0; i < high; i++) {
+			if (arr[i] <= lastelement) {
+				// swapping of i with idx
+				int T = arr[i];
+				arr[i] = arr[idx];
+				arr[idx] = T;
+				idx++;
+			}
+		}
+		// swapping of last element with idx
+		int T = arr[idx];
+		arr[idx] = arr[high];
+		arr[high] = T;
+		return idx;
 	}
 
 }
