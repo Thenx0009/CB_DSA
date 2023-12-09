dc4b5bf86eaeb62c681f8cd52f0bef3c6f0c9728
Thenx0009 <ayushverm0009@gmail.com>
2023-12-09 21:44:53 +0530

    initial commit

--- Leetcode/Binary_Tree_Inorder_Traversal.java
+++ Leetcode/Binary_Tree_Inorder_Traversal.java
@@ -1,10 +1,43 @@
 package Leetcode;
 
+import java.util.*;
+
 public class Binary_Tree_Inorder_Traversal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
 
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		public List<Integer> inorderTraversal(TreeNode root) {
+			ArrayList<Integer> result = new ArrayList<>();
+			inorderTraversalHelper(root, result);
+			return result;
+		}
+
+		private void inorderTraversalHelper(TreeNode root, List<Integer> result) {
+			if (root == null) {
+				return;
+			}
+			inorderTraversalHelper(root.left, result);
+			result.add(root.val);
+			inorderTraversalHelper(root.right, result);
+		}
 	}
 
 }
