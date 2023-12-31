f4d25fdf81ac22a70e43179186456db3ac3bdb29
Thenx0009 <ayushverm0009@gmail.com>
2023-12-31 18:18:52 +0530

    first commit

---
+++ Leetcode/Largest_Substring_Between_Two_Equal_Characters.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class Largest_Substring_Between_Two_Equal_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "aa";
+		System.out.println(maxLengthBetweenEqualCharacters(s));
+	}
+
+	public static int maxLengthBetweenEqualCharacters(String s) {
+		int ans = -1;
+		for (int left = 0; left < s.length(); left++) {
+			for (int right = left + 1; right < s.length(); right++) {
+				if (s.charAt(left) == s.charAt(right)) {
+					ans = Math.max(ans, right - left - 1);
+				}
+			}
+		}
+		return ans;
+	}
+
+}
