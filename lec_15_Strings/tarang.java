2b232e0fdb1fb72602070e3dbb7c9415fd4f0ead
Thenx0009 <ayushverm0009@gmail.com>
2024-01-09 18:19:21 +0530

    initial commit

--- lec_15_Strings/tarang.java
+++ lec_15_Strings/tarang.java
@@ -1,25 +1,25 @@
-//Have the function Questions Marks(str)
-//take the str string parameter, which will
-//contain single digit numbers, letters, and
-//question marks, and check if there are
-//exactly 3 question marks between every
-//pair of two numbers that add up to 10. If
-//so, then your program should return the
-//string true, otherwise it should return the
-//string false. If there aren't any two
-//numbers that add up to 10 in the string,
-//then your program should return false as
-//well. For example: if str is
-//"arrb6???4xxbl5???eee5" then your
-//program should return true because
-//there are exactly 3 question marks
-//between 6 and 4, and 3 question marks
-//between 5 and 5 at the end of the string.
-//Examples
-//
-//Input: "aa6?9"
-//Output: false
+/*Have the function Questions Marks(str)
+take the str string parameter, which will
+contain single digit numbers, letters, and
+question marks, and check if there are
+exactly 3 question marks between every
+pair of two numbers that add up to 10. If
+so, then your program should return the
+string true, otherwise it should return the
+string false. If there aren't any two
+numbers that add up to 10 in the string,
+then your program should return false as
+well. For example: if str is
+"arrb6???4xxbl5???eee5" then your
+program should return true because
+there are exactly 3 question marks
+between 6 and 4, and 3 question marks
+between 5 and 5 at the end of the string.
+Examples
 
+Input: "aa6?9"
+Output: false
+*/
 package lec_15_Strings;
 
 public class tarang {
