88e4df864d65bd2e12e5df69d39d6eea45bc620c
Thenx0009 <ayushverm0009@gmail.com>
2024-02-10 12:47:56 +0530

    first commit

---
+++ Leetcode/Palindromic_Substrings.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+public class Palindromic_Substrings {
+	 static int result = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "aaa";
+		System.out.println(countSubstrings(s));
+	}
+
+	public static int countSubstrings(String s) {
+		result = 0;// Reset result before each call
+		for (int i = 0; i < s.length(); i++) {
+			count(s,i,i);
+			count(s,i,i+1);
+		}
+		return result;
+	}
+	public static void count(String s, int start, int end) {
+		while(start>=0 && end<s.length() && s.charAt(start)==s.charAt(end)) {
+			result++;
+			start--;
+			end++;
+		}
+	}
+
+}
