5a4683550cb2b5d31aeb72023e1cd610f1d3a342
Thenx0009 <ayushverm0009@gmail.com>
2024-02-21 22:33:51 +0530

    initial commit

---
+++ Bit_Manipulation/Bitwise_AND_of_Numbers_Range.java
@@ -0,0 +1,20 @@
+package Bit_Manipulation;
+
+public class Bitwise_AND_of_Numbers_Range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int left = 5;
+		int right = 7;
+		System.out.println(rangeBitwiseAnd(left,right));
+	}
+	public static int rangeBitwiseAnd(int left, int right) {
+        int ans = right;
+        while(right>left){
+            ans = right & (right-1);
+            right = ans;
+        } 
+        return ans;
+    }
+
+}
